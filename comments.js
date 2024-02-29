6fd1e1842fcede775d18aac1177c0ad242a049a1
DonCurrie <93970721+TheDonZA@users.noreply.github.com>
2024-02-29 08:42:02 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,41 @@
-// create web server
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const comments = require('./comments');
-const fs = require('fs');
+// Create web server with Node.js
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Load the http module to create an http server.
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
 
-app.get('/comments', (req, res) => {
-    res.json(comments);
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  var pathname = url.parse(request.url).pathname;
+  console.log('Request for ' + pathname + ' received.');
+
+  if (pathname === '/') {
+    pathname = '/index.html';
+  }
+
+  var extname = path.extname(pathname);
+  var contentType = 'text/html';
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+  }
+
+  fs.readFile(pathname.substr(1), function (err, data) {
+    if (err) {
+      console.log(err);
+      response.writeHead(404, { 'Content-Type': 'text/html' });
+    } else {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.write(data.toString());
+    }
+    response.end();
+  });
 });
 
-app.post('/comments', (req, res) => {
-    const newComment = {
-        id: comments.length + 1,
-    };
-}); // Add closing curly brace here
+// Listen on port 8000, IP defaults to
